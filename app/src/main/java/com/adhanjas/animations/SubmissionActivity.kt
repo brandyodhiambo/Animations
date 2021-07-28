610391807b03fa23b44340f5195c261b6989226f
brandy-kay <brandyariana99@gmail.com>
2021-07-28 13:33:01 +0300

    initial commit

---
+++ app/src/main/java/com/adhanjas/animations/SubmissionActivity.kt
@@ -0,0 +1,11 @@
+package com.adhanjas.animations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubmissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submission)
+    }
+}
